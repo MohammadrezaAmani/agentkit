dbcb888b2dc12529ce6921c5f36c41265be036c0
Mohammadreza Amani <More.Amani@yahoo.com>
2025-07-12 15:17:04 +0330

    initial project

--- scripts/change.py
+++ scripts/change.py
@@ -1,18 +1,20 @@
+# create project structure for agentkit
+
 import os
 import pathlib
 from os import walk
 
-path = pathlib.Path(__file__).parent.parent / "agentkit"
 
+if __name__ == "__main__":
+    path = pathlib.Path(__file__).parent.parent / "agentkit"
 
-# i wanna change all files, delete their data and write from agents.path.of.same.exact.file import * # noqa: F401, F403
-# agents.path.of.same.exact.file must be the same as the file name in python import style
-
-for root, dirs, files in walk(path):
-    for file in files:
-        if file.endswith(".py") and not file.startswith("__init__"):
-            file_path = os.path.join(root, file)
-            with open(file_path, "w") as f:
-                relative_path = pathlib.Path(file_path).relative_to(path)
-                import_path = relative_path.with_suffix("").as_posix().replace("/", ".")
-                f.write(f"from agents.{import_path} import * # noqa: F401, F403\n")
+    for root, dirs, files in walk(path):
+        for file in files:
+            if file.endswith(".py") and not file.startswith("__init__"):
+                file_path = os.path.join(root, file)
+                with open(file_path, "w") as f:
+                    relative_path = pathlib.Path(file_path).relative_to(path)
+                    import_path = (
+                        relative_path.with_suffix("").as_posix().replace("/", ".")
+                    )
+                    f.write(f"from agents.{import_path} import * # noqa: F401, F403\n")
